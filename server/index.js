0a68517d0106aecca8d722fd1396158e13e3164c
Wenny Hidayat <wenny.h@live.com>
2019-11-24 10:46:00 +1100

    Removed body-parser bc already in express

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(bodyParser.json());
 
 // Add in the routes
 require('./routes/property')(app);
