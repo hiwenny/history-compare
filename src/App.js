9041c051cbf0d9ade9388db470a68cbfaa632a9f
Wenny Hidayat <wenny.h@live.com>
2019-12-22 15:39:12 +1100

    Added boundary, removed primereact

--- src/App.js
+++ src/App.js
@@ -1,88 +1,16 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import axios from 'axios';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
 import './App.css';
 
+import ErrorBoundary from './Components/ErrorBoundary';
+import SampleBuggyCounter from './Components/SampleBuggyCounter';
+
 function App() {
-  // remember to put in propertyId as key to prevent rerendering**
-  const sampleData = [
-    {
-      brand: 'VW',
-      year: 2012,
-      color: 'Orange',
-      vin: 'dsad231ff',
-    },
-    {
-      brand: 'Audi',
-      year: 2011,
-      color: 'Black',
-      vin: 'gwregre345',
-    },
-    {
-      brand: 'Renault',
-      year: 2005,
-      color: 'Gray',
-      vin: 'h354htr',
-    },
-    {
-      brand: 'BMW',
-      year: 2003,
-      color: 'Blue',
-      vin: 'j6w54qgh',
-    },
-    {
-      brand: 'Mercedes',
-      year: 1995,
-      color: 'Orange',
-      vin: 'hrtwy34',
-    },
-    {
-      brand: 'Volvo',
-      year: 2005,
-      color: 'Black',
-      vin: 'jejtyj',
-    },
-    {
-      brand: 'Honda',
-      year: 2012,
-      color: 'Yellow',
-      vin: 'g43gr',
-    },
-    {
-      brand: 'Jaguar',
-      year: 2013,
-      color: 'Orange',
-      vin: 'greg34',
-    },
-    {
-      brand: 'Ford',
-      year: 2000,
-      color: 'Black',
-      vin: 'h54hw5',
-    },
-    {
-      brand: 'Fiat',
-      year: 2013,
-      color: 'Red',
-      vin: '245t2s',
-    },
-  ];
-  const sampleShowColumns = [
-    { field: 'vin', header: 'Vin' },
-    { field: 'year', header: 'Year' },
-  ];
   return (
     <div className="App">
       <header className="App-header">
         <input type="text" placeholder="Enter address here..." />
-        {/* sample */}
-        <DataTable value={sampleData}>
-          {sampleShowColumns.map((col) => (
-            <Column key={col.field} field={col.field} header={col.field} />
-          ))}
-        </DataTable>
         <button
           type="button"
           onClick={() => axios
@@ -92,6 +20,9 @@ function App() {
         >
           Test search
         </button>
+        <ErrorBoundary>
+          <SampleBuggyCounter />
+        </ErrorBoundary>
         <button
           type="button"
           onClick={() => axios.get('/search?address=wrong%20street')
---
+++ src/Components/ErrorBoundary.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error);
+    this.setState({
+      error,
+      errorInfo,
+    });
+  }
+
+  render() {
+    const { errorInfo, error } = this.state;
+    if (errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {error && error.toString()}
+            <br />
+            {errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }
+}
