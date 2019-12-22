fd72b7c92cea24fc1229273ef272391de49a6308
Wenny Hidayat <wenny.h@live.com>
2019-12-22 18:28:26 +1100

    Cleanup test data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,66 @@ import React from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Divider from '@material-ui/core/Divider';
 import ErrorBoundary from './Components/ErrorBoundary';
 import Table from './Components/Table';
+
+
+// --- sample data
+
+const sample = [
+  ['Frozen yoghurt', 159, 6.0, 24, 4.0],
+  ['Ice cream sandwich', 237, 9.0, 37, 4.3],
+  ['Eclair', 262, 16.0, 24, 6.0],
+  ['Cupcake', 305, 3.7, 67, 4.3],
+  ['Gingerbread', 356, 16.0, 49, 3.9],
+];
+
+function createData(id, dessert, calories, fat, carbs, protein) {
+  return {
+    id, dessert, calories, fat, carbs, protein,
+  };
+}
+
+const rows = [];
+
+// for (let i = 0; i < 1; i += 1) {
+//   const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+//   rows.push(createData(i, ...randomSelection));
+// }
+
+const columns = [
+  {
+    width: 200,
+    label: 'Dessert',
+    dataKey: 'dessert',
+  },
+  {
+    width: 120,
+    label: 'Calories\u00A0(g)',
+    dataKey: 'calories',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Fat\u00A0(g)',
+    dataKey: 'fat',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Carbs\u00A0(g)',
+    dataKey: 'carbs',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Protein\u00A0(g)',
+    dataKey: 'protein',
+    numeric: true,
+  },
+];
+
+// ---- end of sample data
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,7 +97,7 @@ function App() {
           }}
         />
         <hr />
-        <Table />
+        <Table rows={rows} columns={columns} />
         <button type="button" onClick={() => { throw new Error('Something'); }}>Try error throwing</button>
         <button
           type="button"
