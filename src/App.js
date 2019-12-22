e0d7c04c794ee2ebfa6b6ff19a35e79336a48e24
Wenny Hidayat <wenny.h@live.com>
2019-12-22 15:54:38 +1100

    Trimmed down Boundary component because v16 unmounts all for error stack trace

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import axios from 'axios';
-import './App.css';
-
 import ErrorBoundary from './Components/ErrorBoundary';
-import SampleBuggyCounter from './Components/SampleBuggyCounter';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <input type="text" placeholder="Enter address here..." />
-        <button
-          type="button"
-          onClick={() => axios
-            .get('/search?address=1%20aardvark%20st')
-            .then((res) => console.log('@@ response', res))
-            .catch((err) => console.log(err))}
-        >
+      <ErrorBoundary>
+        <header className="App-header">
+          <input type="text" placeholder="Enter address here..." />
+          <button
+            type="button"
+            onClick={() => axios
+              .get('/search?address=1%20aardvark%20st')
+              .then((res) => console.log('@@ response', res))
+              .catch((err) => console.log(err))}
+          >
           Test search
-        </button>
-        <ErrorBoundary>
-          <SampleBuggyCounter />
-        </ErrorBoundary>
-        <button
-          type="button"
-          onClick={() => axios.get('/search?address=wrong%20street')
-            .catch((err) => console.log(err))}
-
-        >
+          </button>
+          <button
+            type="button"
+            onClick={() => axios.get('/search?address=wrong%20street')
+              .catch((err) => console.log(err))}
+          >
           Test search fail
-        </button>
-        <button
-          type="button"
-          onClick={() => axios
-            .get('/details?propertyId=1')
-            .then((res) => console.log('@@ property details', res))
-            .catch((err) => console.log(err))}
-        >
+          </button>
+          <button
+            type="button"
+            onClick={() => axios
+              .get('/details?propertyId=1')
+              .then((res) => console.log('@@ property details', res))
+              .catch((err) => console.log(err))}
+          >
           Test details
-        </button>
-      </header>
+          </button>
+        </header>
+      </ErrorBoundary>
     </div>
   );
 }
--- src/Components/ErrorBoundary.jsx
+++ src/Components/ErrorBoundary.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
 
 export default class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null, errorInfo: null };
-  }
-
   componentDidCatch(error, errorInfo) {
+    // error reporting here sending stacktrace
+    // ever since react 16 it's futile to try and show a replacement UI, since everything gets unmounted
+    // possibly still useful for reporting - New Relic and the likes.
+    // reportError(error, errorInfo)
     console.error(error);
-    this.setState({
-      error,
-      errorInfo,
-    });
   }
 
   render() {
-    const { errorInfo, error } = this.state;
-    if (errorInfo) {
-      // Error path
-      return (
-        <div>
-          <h2>Something went wrong.</h2>
-          <details style={{ whiteSpace: 'pre-wrap' }}>
-            {error && error.toString()}
-            <br />
-            {errorInfo.componentStack}
-          </details>
-        </div>
-      );
-    }
-    // Normally, just render children
+    // Just render children if all is fine.
     return this.props.children;
   }
 }
