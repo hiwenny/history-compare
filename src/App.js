38fa49b3430101948b07c8272c8605439692c1ab
Wenny Hidayat <wenny.h@live.com>
2020-01-08 22:52:08 +1100

    Cleanup moved API UI test buttons to DevTools

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import ErrorBoundary from './Components/ErrorBoundary';
 import Table from './Components/Table';
 import SearchBar from './Components/SearchBar';
+import DevTools from './DevTools';
 
 // --- sample data
-
 const sample = [
   ['Frozen yoghurt', 159, 6.0, 24, 4.0],
   ['Ice cream sandwich', 237, 9.0, 37, 4.3],
@@ -92,38 +91,7 @@ function App() {
         <SearchBar />
         <hr />
         <Table rows={rows} columns={columns} />
-        <button
-          type="button"
-          onClick={() => {
-            throw new Error('Something');
-          }}
-        >
-          Try error throwing
-        </button>
-        <button
-          type="button"
-          onClick={() => axios
-            .get('/search?address=1%20aardvark%20st')
-            .then((res) => console.log('@@ response', res))
-            .catch((err) => console.error(err))}
-        >
-          Test search
-        </button>
-        <button
-          type="button"
-          onClick={() => axios.get('/search?address=wrong%20street').catch((err) => console.error(err))}
-        >
-          Test search fail
-        </button>
-        <button
-          type="button"
-          onClick={() => axios
-            .get('/details?propertyId=1')
-            .then((res) => console.log('@@ property details', res))
-            .catch((err) => console.error(err))}
-        >
-          Test details
-        </button>
+        <DevTools />
       </ErrorBoundary>
     </div>
   );
--- src/DevTools/index.jsx
+++ src/DevTools/index.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import axios from 'axios';
 
 /** Instead of logging/rendering state, pass them on to here and test.
  * Oh this could be a good use case for HOC!
  *
  * Just enhance the component when developing, remove when done.
  */
-const StatePanel = (state) => {
+const DevTools = (state) => (
   // render state here recursively, key-value pairs
-};
+  <>
+    <button
+      type="button"
+      onClick={() => {
+        throw new Error('Something');
+      }}
+    >
+    Try error throwing
+    </button>
+    <button
+      type="button"
+      onClick={() => axios
+        .get('/search?address=1%20aardvark%20st')
+        .then((res) => console.log('@@ response', res))
+        .catch((err) => console.error(err))}
+    >
+      Test search
+    </button>
+    <button
+      type="button"
+      onClick={() => axios.get('/search?address=wrong%20street').catch((err) => console.error(err))}
+    >
+      Test search fail
+    </button>
+    <button
+      type="button"
+      onClick={() => axios
+        .get('/details?propertyId=1')
+        .then((res) => console.log('@@ property details', res))
+        .catch((err) => console.error(err))}
+    >
+      Test details
+    </button>
+  </>
+);
+
+export default DevTools;
